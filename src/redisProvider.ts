e741f12f4808ea597d9a034eadd815dad0fce18e
referenz <thomas.u.schueler@gmail.com>
2022-10-02 23:56:12 +0200

    Strictes Typing

--- src/redisProvider.ts
+++ src/redisProvider.ts
@@ -3,5 +3,5 @@ import { createClient } from 'redis';
 export function getRedisClient() {
   return process.env.NODE_ENV === 'development'
     ? createClient()
-    : createClient({ socket: { port: 0, path: '/home/referenz/.redis/sock' } });
+    : createClient({ socket: { port: 0, path: '/home/referenz/.redis/sock', timeout: 5000 } });
 }
